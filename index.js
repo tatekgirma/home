3a43e0286ac165ceab6232a59cc4ee08b3ded8c1
Tatek Dese <114894000+tatekgirma@users.noreply.github.com>
2026-08-26 11:53:44 -0500

    Update index.js
    coomits from github

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-Im from wrting this from CLI
-Im from wrting this from CLI
-"After I pushed every thing to ........."
+I'm writing this from the CLI
+I'm writing this from the CLI
+"After I pushed everything to ........."
+I'm editing this from Origin Main.....
